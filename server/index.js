035c6e0436d8e42fb9b30d5c8ffa70bdf3091cc3
Jenil Prajapati <jenilrajeshkumar@ucsb.edu>
2024-08-23 21:25:44 +0400

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+// MongoDB connection string
+const CONNECTION_URL = 'mongodb+srv://prajapatijenil0407:AvoVirW6OiSFimaV@cluster0.nq1ad.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const PORT = process.env.PORT || 5001; // Make sure to use `PORT` in uppercase
+
+//might have to remove the useNewUrlParser and useUnifiedTopology later
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+// Remove this line as `useFindAndModify` is no longer supported
+// mongoose.set('useFindAndModify', false);
