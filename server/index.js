e95a4223242ed829c317f36926c6e9248bcf210f
Jenil Prajapati <jenilrajeshkumar@ucsb.edu>
2024-08-30 15:22:21 +0400

    updated files

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -13,8 +14,12 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
+app.get('/', (req, res) => {
+    res.send('Hello to reminisce API')
+})
+
 // MongoDB connection string
-const CONNECTION_URL = 'mongodb+srv://prajapatijenil0407:AvoVirW6OiSFimaV@cluster0.nq1ad.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+//const CONNECTION_URL = 'mongodb+srv://prajapatijenil0407:AvoVirW6OiSFimaV@cluster0.nq1ad.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const PORT = process.env.PORT || 5001; // Make sure to use `PORT` in uppercase
 
 //might have to remove the useNewUrlParser and useUnifiedTopology later
